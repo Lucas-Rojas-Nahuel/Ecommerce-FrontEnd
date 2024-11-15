e4679da4db73ab680a142524b5399acd3eef20ea
Lucas-Rojas-Nahuel <rolukasro12@gmail.com>
2024-11-15 13:07:21 -0300

    style(sectionDiscount): ajustar botones y agregar estilos responsivos

--- src/components/body/home/sectionCarruselProductsDiscount/sectionDiscount.jsx
+++ src/components/body/home/sectionCarruselProductsDiscount/sectionDiscount.jsx
@@ -1,16 +1,24 @@
- import CarruselProducts from './carruselProducts/carruselProducts'
-import './sectionDiscount.css'
+import CarruselProducts from "./carruselProducts/carruselProducts";
+import "./sectionDiscount.css";
 
 export default function SectionDiscount() {
   return (
     <section className="section-discount">
-        <section className='section-title'>
-            <h3 className='title'>Ofertas</h3>
-            <a href=""><strong>Conocer más Ofertas</strong></a>
-        </section>
-        <section className='section-tarject-products'>
-            <CarruselProducts/>
-        </section>
+      <section className="section-title">
+        <h3 className="title">Ofertas</h3>
+        <a href="">
+          <strong>Conocer más Ofertas</strong>
+        </a>
+      </section>
+      <section className="section-tarject-products">
+        <CarruselProducts />
+      </section>
+      <section className="section-link">
+        <a href="">
+          <strong>Conocer más Ofertas</strong>
+          <i className="fi fi-rr-angle-small-right"></i>
+        </a>
+      </section>
     </section>
-  )
+  );
 }
